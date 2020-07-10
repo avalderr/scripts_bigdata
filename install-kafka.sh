b0f2cf2cd6c1dc96ca8a54714f9824f08af4394d
avalderr <alvaro.valderrama.13@sansano.usm.cl>
2020-07-10 07:21:06 +0000

    script tentativo para instalar kafka en instancia limpia. DEBE CORRERSE CON SUDO. Crea igualmente el servicio y lo deja comenzado y enabled

---
+++ install-kafka.sh
@@ -0,0 +1,19 @@
+#/bin/bash!
+apt-get update 
+yes | apt-get install default-jre
+yes | apt-get install zookeeperd
+adduser --system --no-create-home --disabled-password --disabled-login kafka
+mkdir ~/inst
+cd ~/inst
+wget https://downloads.apache.org/kafka/2.5.0/kafka_2.12-2.5.0.tgz
+mkdir /opt/kafka
+tar -xvzf kafka_2.12-2.5.0.tgz --directory /opt/kafka --strip-components 1
+mkdir /var/lib/kafka
+mkdir /var/lib/kafka/data
+mkdir /var/lib/kafka
+mkdir /var/lib/kafka/data
+chown -R kafka:nogroup /opt/kafka
+chown -R kafka:nogroup /var/lib/kafka
+cp ~/scripts/kafka.service  /etc/systemd/system/
+systemctl start kafka.service
+systemctl enable kafka.service
