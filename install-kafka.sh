44426c326ca61335e4395c43184bce74aaf30b3e
Ubuntu <ubuntu@ip-172-31-61-239.ec2.internal>
2020-07-15 21:02:46 +0000

    se actualizan pequeñas direcciones para coincidir con el nombre del repositorio

--- install-kafka.sh
+++ install-kafka.sh
@@ -8,13 +8,13 @@ cd ~/inst
 wget https://downloads.apache.org/kafka/2.5.0/kafka_2.12-2.5.0.tgz
 mkdir /opt/kafka
 tar -xvzf kafka_2.12-2.5.0.tgz --directory /opt/kafka --strip-components 1
-cp ~/scripts/confs/server.properties /opt/kafka/config
+cp ~/scripts_bigdata/confs/server.properties /opt/kafka/config
 mkdir /var/lib/kafka
 mkdir /var/lib/kafka/data
 mkdir /var/lib/kafka
 mkdir /var/lib/kafka/data
 chown -R kafka:nogroup /opt/kafka
 chown -R kafka:nogroup /var/lib/kafka
-cp ~/scripts/confs/kafka.service  /etc/systemd/system/
+cp ~/scripts_bigdata/confs/kafka.service  /etc/systemd/system/
 systemctl start kafka.service
 systemctl enable kafka.service
