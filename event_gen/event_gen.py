ac12c9c7d3b681ae707dcb8ceebd98fb503bd77a
root <root@instance-3.us-central1-a.c.daring-slice-283816.internal>
2020-08-02 06:22:36 +0000

    se cambia a librería de kafka que es mil mil millones de veces más eficiente

--- event_gen/event_gen.py
+++ event_gen/event_gen.py
@@ -11,6 +11,7 @@ import subprocess
 import time
 import datetime
 from sys import argv
+from kafka import KafkaProducer
 
 arg2 = int(argv[1])
 #day_fraction = arg2 if arg2 <= 1 else 1/arg2
@@ -21,26 +22,22 @@ df = pd.read_csv(os.path.join('data_crunched','trade_data_01-03-2020.csv'),nrows
 df.loc[:,'TIME'] = pd.to_datetime(df.TIME)
 df.loc[:,'delta'] = (df.TIME - df.iloc[0].TIME).apply(lambda x: x.seconds/float(relative_speed))
 
-run_scripts = ['/bin/echo', '', '|', '/opt/kafka/bin/kafka-console-producer.sh', '--broker-list', 'localhost:9092', '--topic', '', '>', '/dev/null']
+prod = KafkaProducer(bootstrap_servers='localhost:9092')#,value_serializer=str.encode)
 
-def exec_thing(string, topic):
-    run_scripts[1] = '"'+string[0]+' '+str(string[1])+ '"'
-    run_scripts[7] = topic
-    print(' '.join(run_scripts))
-    # subprocess.Popen(['echo',str(run_scripts)])
-    s = subprocess.Popen(' '.join(run_scripts),shell=True)
-    print(s)
+def exec_thing(string, topic,prod):
+    print(str(string))
+    prod.send(topic,str(string))
     return
     
-def create_sched(s, x):
+def create_sched(s, x, prod):
     string = x[['Mnemonic','Price']].values
-    s.enter(x.delta,x.ind,exec_thing,(string,x.Topic))
+    s.enter(x.delta,x.ind,exec_thing,(string,x.Topic,prod))
     return 
 
 s = sched.scheduler(time.time, time.sleep)
 #df.iloc[210:230].apply(lambda x: create_sched(s,x), axis=1)
 
-df.apply(lambda x: create_sched(s,x), axis=1)
+df.apply(lambda x: create_sched(s,x,prod), axis=1)
 lalal = raw_input("RUN NOW?")
 s.run()
 print("FINISHED?!?!?!?")
