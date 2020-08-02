977d3c30771d65e2ec0a8b10c75e05ca42b00957
root <root@instance-3.us-central1-a.c.daring-slice-283816.internal>
2020-08-02 06:20:30 +0000

    se agrega instalacion de librería kafka

--- ready_all.sh
+++ ready_all.sh
@@ -4,7 +4,8 @@ cd /root
 apt install unzip
 unzip event_gen/trade_data_01-03-2020.zip -d event_gen/data_crunched/
 yes | apt install virtualenv
-virtualenv eventgen 
-source eventgen/bin/activate
+virtualenv env 
+source env/bin/activate
 pip install pandas 
+pip install kafka-python
 echo 'Esta TODO OK, se puede correr event gen'
