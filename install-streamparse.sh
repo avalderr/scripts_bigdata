19f9666ec8a1732df82b187eda1fd1831b61dd1d
root <root@instance-6.us-central1-a.c.daring-slice-283816.internal>
2020-08-24 03:43:22 +0000

    typo

--- install-streamparse.sh
+++ install-streamparse.sh
@@ -7,7 +7,7 @@ chmod +x /bin/lein
 lein
 
 wget https://downloads.apache.org/storm/apache-storm-1.2.3/apache-storm-1.2.3.zip
-uzip apache-storm-1.2.3.zip
+unzip apache-storm-1.2.3.zip
 echo 'export PATH="/root/scripts_bigdata/apache-storm-1.2.3/bin/:$PATH"' >> /root/.bashrc
 exec bash --login
 
