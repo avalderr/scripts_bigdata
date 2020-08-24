c749df3dbdb1ead8bc95e2b65df9a0c37b5d8c1e
root <root@instance-4.us-central1-a.c.daring-slice-283816.internal>
2020-08-24 06:02:12 +0000

    se permite elegir tamaño de batch para carga parcial de datos

--- event_gen/event_gen.py
+++ event_gen/event_gen.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
-# python event_gen.py n_examples speed
+# python event_gen.py n_examples speed batch_size
 # In[ ]:
 
 
@@ -16,12 +16,13 @@ from kafka import KafkaProducer
 arg2 = int(argv[1])
 #day_fraction = arg2 if arg2 <= 1 else 1/arg2
 relative_speed = argv[2]
+batch = int(argv[3])
 
-df = pd.read_csv(os.path.join('data_crunched','trade_data_01-03-2020.csv'),nrows=arg2).rename(columns={'Unnamed: 0':'ind'})
+df = pd.read_csv(os.path.join('data_crunched','trade_data_01-03-2020.csv'),nrows=10,skiprows=1, names=['ind','Mnemonic','Price','TIME','Topic'])#.rename(columns={'Unnamed: 0':'ind'})
+
 #df = df.iloc[:int(df.shape[0]*day_fraction)]
 df.loc[:,'TIME'] = pd.to_datetime(df.TIME)
-df.loc[:,'delta'] = (df.TIME - df.iloc[0].TIME).apply(lambda x: x.seconds/float(relative_speed))
-
+start_time = df.iloc[0].TIME
 prod = KafkaProducer(bootstrap_servers='localhost:9092')#,value_serializer=str.encode)
 
 def exec_thing(string, topic,prod):
@@ -35,9 +36,16 @@ def create_sched(s, x, prod):
     return 
 
 s = sched.scheduler(time.time, time.sleep)
+for i in range(int(arg2/batch)):
+    skip = i*batch+1
+    nrows = batch
+
+    df = pd.read_csv(os.path.join('data_crunched','trade_data_01-03-2020.csv'),nrows=nrows,skiprows=skip, names=['ind','Mnemonic','Price','TIME','Topic'])#.rename(columns={'Unnamed: 0':'ind'})
+    df.loc[:,'TIME'] = pd.to_datetime(df.TIME)
+    df.loc[:,'delta'] = (df.TIME - start_time).apply(lambda x: x.seconds/float(relative_speed))
+
 #df.iloc[210:230].apply(lambda x: create_sched(s,x), axis=1)
 
-df.apply(lambda x: create_sched(s,x,prod), axis=1)
+    df.apply(lambda x: create_sched(s,x,prod), axis=1)
 lalal = raw_input("RUN NOW?")
 s.run()
-print("FINISHED?!?!?!?")
